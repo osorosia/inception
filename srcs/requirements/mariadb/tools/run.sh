1ea61e23d90dbdacd17518b6048cd930cb7b1922
osorosia <64416398+osorosia@users.noreply.github.com>
2023-03-12 06:00:49 +0900

    simplify

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -4,8 +4,8 @@ if [ ! -d "/run/mysqld" ]; then
 	mkdir -p /run/mysqld
 fi
 chown -R mysql:mysql /run/mysqld
-
 chown -R mysql:mysql /var/lib/mysql
+
 if [ ! -d /var/lib/mysql/mysql ]; then
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql >/dev/null
 
