3fdd63d68ad418e921d7e5d1309d4991c09f7fce
osorosia <64416398+osorosia@users.noreply.github.com>
2023-03-20 06:39:31 +0900

    wordpress

---
+++ srcs/requirements/wordpress/tools/run.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ ! -f wp-config.php ]; then
+  cp -rf /restore/* /var/www/html
+fi
+
+echo "Connecting database..."
+while ! mariadb -h$WORDPRESS_DB_HOST -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME 2>/dev/null; do
+  sleep 1
+done
+
+cat << EOF | mariadb -h$WORDPRESS_DB_HOST -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME | grep wp_users > /dev/null
+show tables;
+EOF
+result=$?
+
+if [ $result -ne 0 ]; then
+  echo "Installing Wordpress..."
+  wp core install \
+    --url=localhost:8081 \
+    --title=rnishimo_blog \
+    --admin_name=kanrisha \
+    --admin_password=a \
+    --admin_email=kanrisha@example.com \
+    --path=/var/www/html \
+    --allow-root
+  wp user create hutu hutu@example.com \
+    --user_pass=a \
+    --role=editor \
+    --allow-root
+fi
+
+echo "Wordpress started!"
+/usr/sbin/php-fpm7 -F
